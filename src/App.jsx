b4d62947a717381e75061096f3414915e0efbece
YuHeng <codeingyeh@gmail.com>
2025-03-24 00:23:23 +0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 import { useState } from "react";
 
+//取得時間，預設回傳當前時間
+function getDateTime(hourOffset = 0) {
+  const now = new Date();
+  now.setMinutes(0);
+  now.setSeconds(0);
+  now.setMilliseconds(0);
+  now.setHours(now.getHours() + hourOffset);
+  // 回傳yyyy-MM-ddThh:mm:ss
+  return now.toISOString().replace(/\.\d+Z$/, ":00");
+}
+
 function App() {
   return <div>天氣</div>;
 }
