80a2d31223a1e4b286f34c1f7ca20c74c21fae84
YuHeng <codeingyeh@gmail.com>
2025-03-25 00:03:55 +0800

    commit

--- src/hooks/UseFetch.jsx
+++ src/hooks/UseFetch.jsx
@@ -1,27 +1,33 @@
 import { useEffect, useState } from "react";
 
-function useFetch(url) {
+export default function useFetch(url) {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const controller = new AbortController();
     const fetchData = async () => {
       try {
-        const response = await fetch(url);
+        const response = await fetch(url, {
+          signal: controller.signal,
+        });
         if (!response.ok) {
           throw new Error("請求失敗");
         }
         const result = await response.json();
         setData(result);
       } catch (err) {
-        setError(err.message);
+        if (err.name !== "AbortError") {
+          setError(err.message);
+        }
       } finally {
         setLoading(false);
       }
     };
 
     fetchData();
+    return () => controller.abort();
   }, [url]);
 
   return { data, loading, error };
---
+++ src/pages/weather.jsx
@@ -0,0 +1,4 @@
+import { useState, useEffect } from "react";
+import { useFetch } from "../hooks/useFetch";
+
+export default function Waether() {}
