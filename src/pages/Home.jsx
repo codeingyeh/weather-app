b190a3b32aab9c48d1be408d834395745fef6bb9
YuHeng <codeingyeh@gmail.com>
2025-04-01 00:30:12 +0800

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,10 @@
+import useLocalStorage from "../hooks/useLocalStorage";
+
 export default function Home() {
+  const [subscript, setSubscript] = useLocalStorageState(
+    [],
+    "subscriptionDistricts"
+  );
+
   return <div>天氣</div>;
 }
